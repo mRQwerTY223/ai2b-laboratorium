c6a991f8911e0e7b5e75ff60bc8a87f1a897f251
Marek Zydor <zm49458@zut.edu.pl>
2023-11-27 02:36:03 +0100

    lab-a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./home/home.component";
+import routeConfig from "./routes";
+import {provideRouter, RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeComponent,
+    RouterModule,
   ],
-  providers: [],
+  providers: [provideRouter(routeConfig)],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
